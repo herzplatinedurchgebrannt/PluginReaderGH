d6d4e5d3e6816c0ff353a486568f96c0a31f79fd
Alexander Mathieu <alexandermathieu@googlemail.com>
2019-12-21 17:34:24 +0100

    creating objects works, browsing/ searching for dll files works

---
+++ PluginReaderVS/PluginReaderVS/BrowsePlugins.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using delegateCommand.Model;
+using Ookii.Dialogs.Wpf;
+
+namespace PluginReaderVS
+{
+    class BrowsePlugins : INotifyPropertyChanged
+    {
+
+        public BrowsePlugins()
+        {
+            this.pluginPositions = new ObservableCollection<VSTPlugin>();
+        }
+
+
+        private ObservableCollection<VSTPlugin> _pluginPositions;
+        public ObservableCollection<VSTPlugin> pluginPositions
+        {
+            get { return _pluginPositions; }
+            set
+            {
+                if (value != _pluginPositions)
+                {
+                    _pluginPositions = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+
+
+
+
+
+        private DelegateCommand _browseCommand;
+        public DelegateCommand browseCommand
+        {
+            get
+            {
+                if (_browseCommand == null)
+                {
+                    _browseCommand = new DelegateCommand(browsePlugins,
+
+                        () =>
+                        {
+                            return true;
+                        }
+                        );
+                }
+                return _browseCommand;
+            }
+        }
+
+
+        void browsePlugins()
+        {
+            Ookii.Dialogs.Wpf.VistaFolderBrowserDialog fbd = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            Nullable<bool> dialogResult = fbd.ShowDialog();
+
+            if (dialogResult == true)
+            {
+                try
+                {
+                    string[] pathRootVST = Directory.GetFiles(fbd.SelectedPath.ToString(), "*.dll");
+
+                    if (pathRootVST.Length > 0)
+                    {
+                        foreach (string vst in pathRootVST)
+                        {
+                            pluginPositions.Add(new VSTPlugin()
+                            {
+                                name = "hall0",
+                                type = "irgendwas",
+                                path = "hei und lo",
+                            });
+                        }
+
+
+
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Fehler catch");
+                }
+            }
+
+
+
+        }
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Private Helpers
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+
+            if (PropertyChanged != null)
+            {
+
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ PluginReaderVS/PluginReaderVS/VSTPlugin.cs
@@ -0,0 +1,87 @@
+﻿using delegateCommand.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PluginReaderVS
+{
+    public class VSTPlugin : INotifyPropertyChanged
+    {
+
+        private string _path;
+
+        public string path
+        {
+            get { return _path; }
+            set
+            {
+                _path = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        private string _name;
+
+        public string name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        private string _type;
+
+        public string type
+        {
+            get { return _type; }
+            set
+            {
+                _type = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+
+
+
+
+
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Private Helpers
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+
+            if (PropertyChanged != null)
+            {
+
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+            }
+        }
+        #endregion
+
+
+    }
+
+
+
+}
